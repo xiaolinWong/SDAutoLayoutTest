c3715227fc162d9d05d4c85a3b39272c75dff409
xiaolinWang <wxl_vipemail@163.com>
2018-11-12 08:53:24 +0800

    first Commiit

--- SDAutoLayoutTest/XLKnowLedgeInfo.h
+++ SDAutoLayoutTest/XLKnowLedgeInfo.h
@@ -9,17 +9,17 @@
 #import <Foundation/Foundation.h>
 
 @interface XLKnowLedgeInfo : NSObject
-@property (nonatomic, strong) NSString *id;
-@property (nonatomic, strong) NSString *index;
-@property (nonatomic, strong) NSString *title;
-@property (nonatomic, strong) NSString *descriptions;
-@property (nonatomic, strong) NSString *img_cover;
-@property (nonatomic, strong) NSString *zan_num;
-@property (nonatomic, strong) NSString *comment_num;
-@property (nonatomic, strong) NSString *zaned;
-@property (nonatomic, strong) NSString *short_title;
-@property (nonatomic, strong) NSString *author;
-@property (nonatomic, strong) NSString *url;
-@property (nonatomic, strong) NSString *add_time;
+@property (nonatomic, copy) NSString *id;
+@property (nonatomic, copy) NSString *index;
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *descriptions;
+@property (nonatomic, copy) NSString *img_cover;
+@property (nonatomic, copy) NSString *zan_num;
+@property (nonatomic, copy) NSString *comment_num;
+@property (nonatomic, copy) NSString *zaned;
+@property (nonatomic, copy) NSString *short_title;
+@property (nonatomic, copy) NSString *author;
+@property (nonatomic, copy) NSString *url;
+@property (nonatomic, copy) NSString *add_time;
 
 @end
